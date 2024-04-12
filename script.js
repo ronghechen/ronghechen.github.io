f0aea30b29b98b11c9a506b7d64e10f82e109364
ronghe chen <102492211+ronghechen@users.noreply.github.com>
2024-04-12 11:39:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 window.addEventListener("load", () => {
   const loader = document.querySelector(".loader");
-  const loaderWrapper = document.querySelector(".loader-wrapper");
   loader.classList.add("loader-hidden");
-  loaderWrapper.classList.add("loader-wrapper-hidden");
-
   loader.addEventListener("transitioned", () => {
     document.body.removeChild("loader");
   })
+})
+
+window.addEventListener("loadingScreen", () => {
+  const loaderWrapper = document.querySelector(".loader-wrapper");
+  loaderWrapper.classList.add("loader-wrapper-hidden");
   loaderWrapper.addEventListener("transitioned", () => {
     document.body.removeChild("loaderWrapper");
   })
