50918a5ae141e029c52b64af9fcf53b8a5965bff
ronghe chen <102492211+ronghechen@users.noreply.github.com>
2024-10-17 10:00:08 -0500

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,33 +1,23 @@
-const track = document.querySelector('.carousel-track');
-const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel-button-right');
-const prevButton = document.querySelector('.carousel-button-left');
-const slideWidth = slides[0].getBoundingClientRect().width;
+let slideIndex = 0;
+showSlides(slideIndex);
 
-slides.forEach((slide, index) => {
-  slide.style.left = slideWidth * index + 'px';
-});
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-const moveToSlide = (track, currentSlide, targetSlide) => {
-  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-  currentSlide.classList.remove('current-slide');
-  targetSlide.classList.add('current-slide');
-};
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slides");
 
-nextButton.addEventListener('click', () => {
-  const currentSlide = track.querySelector('.current-slide');
-  const nextSlide = currentSlide.nextElementSibling;
-
-  if (nextSlide) {
-    moveToSlide(track, currentSlide, nextSlide);
+  if (n >= slides.length) {
+    slideIndex = 0;
+  } 
+  if (n < 0) {
+    slideIndex = slides.length - 1;
   }
-});
 
-prevButton.addEventListener('click', () => {
-  const currentSlide = track.querySelector('.current-slide');
-  const prevSlide = currentSlide.previousElementSibling;
-
-  if (prevSlide) {
-    moveToSlide(track, currentSlide, prevSlide);
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-});
+
+  slides[slideIndex].style.display = "block";  
+}
